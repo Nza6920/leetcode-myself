5b5663c9643568450171268c75ba4f9c605b7b90
2388426660 <2388426660@qq.com>
2019-05-15 08:48:23 +0800

    移动零

---
+++ src/leet20190515/Solution.java
@@ -0,0 +1,45 @@
+package leet20190515;
+
+import com.sun.deploy.util.ArrayUtil;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+ *
+ * 示例:
+ *
+ * 输入: [0,1,0,3,12]
+ * 输出: [1,3,12,0,0]
+ *
+ * 说明:
+ *
+ *     必须在原数组上操作，不能拷贝额外的数组。
+ *     尽量减少操作次数。
+ */
+public class Solution {
+    /**
+     * 利用额外的空间
+     * @param nums 目标数组
+     */
+    public static void moveZeroes(int[] nums) {
+        int[] numsCopy = Arrays.copyOf(nums, nums.length);
+        int flag = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (numsCopy[i] != 0) {
+                nums[flag++] = numsCopy[i];
+            }
+        }
+
+        for (int j = flag; j < nums.length; j++) {
+            nums[j] = 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0,1,0,3,12};
+        moveZeroes(nums);
+    }
+}
