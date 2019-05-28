834643aede412749c71dd2ab4aa412871cf1fa0b
2388426660 <2388426660@qq.com>
2019-05-28 11:09:35 +0800

    相同的树

---
+++ src/leet20190528/Solution.java
@@ -0,0 +1,80 @@
+package leet20190528;
+
+import javax.swing.tree.TreeNode;
+import javax.xml.transform.Source;
+
+/**
+ * 给定两个二叉树，编写一个函数来检验它们是否相同。
+ *
+ * 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+ *
+ * 示例 1:
+ *
+ * 输入:       1         1
+ *           / \       / \
+ *          2   3     2   3
+ *
+ *         [1,2,3],   [1,2,3]
+ *
+ * 输出: true
+ *
+ * 示例 2:
+ *
+ * 输入:      1          1
+ *           /           \
+ *          2             2
+ *
+ *         [1,2],     [1,null,2]
+ *
+ * 输出: false
+ *
+ * 示例 3:
+ *
+ * 输入:       1         1
+ *           / \       / \
+ *          2   1     1   2
+ *
+ *         [1,2,1],   [1,1,2]
+ *
+ * 输出: false
+ */
+
+
+
+public class Solution {
+    /**
+     * 递归
+     * @param p
+     * @param q
+     * @return boolean
+     */
+    public static boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (q == null || p == null) return false;
+        if (p.val != q.val) return false;
+        return isSameTree(p.right, q.right) && isSameTree(p.left, q.left);
+    }
+
+    public static void main(String[] args) {
+        TreeNode parent1 = new TreeNode(1);
+        parent1.left = new TreeNode(2);
+        parent1.right = new TreeNode(3);
+
+        TreeNode parent2 = new TreeNode(1);
+        parent2.left = new TreeNode(2);
+        parent2.right = new TreeNode(3);
+
+        System.out.println(isSameTree(parent1, parent2));
+
+        System.out.println("finish :)");
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
