d36216ead4e53388ec2fc8db167be3ffa1271117
2388426660 <2388426660@qq.com>
2019-06-10 09:43:30 +0800

    转置矩阵

---
+++ src/leet20190609/Solution.java
@@ -0,0 +1,47 @@
+package leet20190609;
+
+/**
+ *给定一个矩阵 A， 返回 A 的转置矩阵。
+ *
+ * 矩阵的转置是指将矩阵的主对角线翻转，交换矩阵的行索引与列索引。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：[[1,2,3],[4,5,6],[7,8,9]]
+ * 输出：[[1,4,7],[2,5,8],[3,6,9]]
+ *
+ * 示例 2：
+ *
+ * 输入：[[1,2,3],[4,5,6]]
+ * 输出：[[1,4],[2,5],[3,6]]
+ *
+ * 提示：
+ *
+ *     1 <= A.length <= 1000
+ *     1 <= A[0].length <= 1000
+ *
+ */
+public class Solution {
+
+    public static int[][] transpose(int[][] A) {
+
+        int[][] B = new int[A[0].length][A.length];
+
+        for (int i = 0; i < B.length; i++) {
+            for (int j = 0; j < B[i].length; j++) {
+                B[i][j] = A[j][i];
+            }
+        }
+
+        return B;
+    }
+
+    public static void main(String[] args) {
+        int[][] nums = new int[][] {{1, 2, 3}, {4, 5, 6}};
+        int[][] B = transpose(nums);
+
+        System.out.println("finish :)");
+    }
+}
